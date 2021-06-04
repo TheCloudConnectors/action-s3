82b5c531646b0742a7ca4e9f70777440c436a11b
Guillaume Lorquet <guillaumelorquet@live.ca>
2021-06-04 12:36:00 -0400

    Add support for sync/copy commands & rename variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,13 @@
 
 set -e
 
-if [ -z "$AWS_S3_BUCKET" ]; then
-  echo "AWS_S3_BUCKET is not set. Quitting."
+if [ -z "$SOURCE" ]; then
+  echo "SOURCE is not set. Quitting."
   exit 1
 fi
 
-if [ -z "$SOURCE_DIR" ]; then
-  echo "SOURCE_DIR is not set. Quitting."
-  exit 1
-fi
-
-if [ -z "$DEST_DIR" ]; then
-  echo "DEST_DIR is not set. Quitting."
+if [ -z "$DEST" ]; then
+  echo "DEST is not set. Quitting."
   exit 1
 fi
 
@@ -21,8 +16,12 @@ if [ -z "$AWS_REGION" ]; then
   AWS_REGION="us-east-2"
 fi
 
+if [ -z "$S3_ACTION" ]; then
+  S3_ACTION="cp"
+fi
+
 if [ -z "$EXTRA_ARGS" ]; then
   EXTRA_ARGS=""
 fi
 
-sh -c "aws s3 cp '${SOURCE_DIR:-.}' 's3://${AWS_S3_BUCKET}/${DEST_DIR}' $EXTRA_ARGS"
+sh -c "aws s3 ${S3_ACTION} '${SOURCE:-.}' '${DEST}' $EXTRA_ARGS"
